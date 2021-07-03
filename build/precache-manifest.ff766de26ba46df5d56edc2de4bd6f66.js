87bc548391305676acb06311cb0e7c85f51202dd
Aryaman Kohli <aryamanmartinian@gmail.com>
2021-07-04 04:51:52 +0530

    second commit

---
+++ build/precache-manifest.ff766de26ba46df5d56edc2de4bd6f66.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4968b28dd8a42b6dba3b65e9e1530d43",
+    "url": "/covid19india/index.html"
+  },
+  {
+    "revision": "bf17bf2ff25f5ee84201",
+    "url": "/covid19india/static/css/2.f2c4c274.chunk.css"
+  },
+  {
+    "revision": "92dd52dc8d3eb537361f",
+    "url": "/covid19india/static/css/main.6c216929.chunk.css"
+  },
+  {
+    "revision": "bf17bf2ff25f5ee84201",
+    "url": "/covid19india/static/js/2.ce97c6ba.chunk.js"
+  },
+  {
+    "revision": "e303234b7c98ab07140fda539d130e8b",
+    "url": "/covid19india/static/js/2.ce97c6ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92dd52dc8d3eb537361f",
+    "url": "/covid19india/static/js/main.c87447ec.chunk.js"
+  },
+  {
+    "revision": "851445d2ca1482abe1d4",
+    "url": "/covid19india/static/js/runtime-main.f2fdf7b1.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/covid19india/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/covid19india/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/covid19india/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/covid19india/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/covid19india/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/covid19india/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/covid19india/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/covid19india/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/covid19india/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/covid19india/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/covid19india/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/covid19india/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/covid19india/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/covid19india/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/covid19india/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/covid19india/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/covid19india/static/media/fa-solid-900.ec763292.svg"
+  }
+]);
